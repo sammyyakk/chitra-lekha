cc45131338dfa52aad4bd4869320f51a786945ea
sammyyakk <sammyyakk@gmail.com>
2025-02-24 21:52:29 +0530

    final push v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+  title: "Chitra Lekha",
+  description: "By Team Semantix",
 };
 
 export default function RootLayout({
